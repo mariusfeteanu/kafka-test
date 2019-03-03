d32c2929ce44a96389f37d5004ed1b426d855933
Marius <marius.feteanu@gmail.com>
2019-03-03 19:20:34 +0000

    multi-broker

--- script/clean.sh
+++ script/clean.sh
@@ -3,5 +3,9 @@ rm -rf ./data/zookeeper/2/version-2
 rm -rf ./data/zookeeper/3/version-2
 
 rm -rf ./data/kafka/1/*
+rm -rf ./data/kafka/2/*
+rm -rf ./data/kafka/3/*
 
 rm -rf ./logs/kafka/1/*
+rm -rf ./logs/kafka/2/*
+rm -rf ./logs/kafka/3/*

1a2e59eea93a898b299fbbc5c9e0be2deebc5ee8
mariusfeteanu <marius.feteanu@gmail.com>
2019-03-04 13:31:36 +0000

    pin an archived version of zookeeper

--- build/zookeeper.sh
+++ build/zookeeper.sh
@@ -1,5 +1,5 @@
 cd /root
-wget -q http://mirrors.ukfast.co.uk/sites/ftp.apache.org/zookeeper/stable/zookeeper-3.4.12.tar.gz
-tar -zxf zookeeper-3.4.12.tar.gz
-mv zookeeper-3.4.12 /usr/local/zookeeper
+wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz 
+tar -zxf zookeeper-3.4.9.tar.gz
+mv zookeeper-3.4.9 /usr/local/zookeeper
 mkdir -p /var/lib/zookeeper

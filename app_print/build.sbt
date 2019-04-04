66822c6de33e0d1763c349c198b9cee4ce6bd232
mariusfeteanu <marius.feteanu@gmail.com>
2019-04-04 18:43:24 +0100

    try to fix the app

--- app_print/build.sbt
+++ app_print/build.sbt
@@ -4,3 +4,6 @@ scalaVersion := "2.12.0"
 
 // libraryDependencies += "org.apache.kafka" % "kafka-streams" % "2.2.0"
 libraryDependencies += "org.apache.kafka" %% "kafka-streams-scala" % "2.2.0"
+
+
+mainClass in assembly := Some("AppPrint")
--- app_print/src/main/scala/AppPrint.scala
+++ app_print/src/main/scala/AppPrint.scala
@@ -6,18 +6,22 @@ import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 import org.apache.kafka.streams.scala.kstream._
 import org.apache.kafka.streams.scala.StreamsBuilder
 
-object MapFunctionScalaExample extends App {
+// https://github.com/confluentinc/kafka-streams-examples/blob/5.2.1-post/src/main/scala/io/confluent/examples/streams/MapFunctionScalaExample.scala
+object AppPrint extends App {
 
   import org.apache.kafka.streams.scala.Serdes._
   import org.apache.kafka.streams.scala.ImplicitConversions._
 
+  val bootstrapServers = args(0)
+  println(s"Starting print app, using $bootstrapServers")
+
   val config: Properties = {
     val p = new Properties()
     p.put(StreamsConfig.APPLICATION_ID_CONFIG, "app_print")
-    val bootstrapServers = args(0)
     p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
     p
   }
+
 
   val builder = new StreamsBuilder
   val textLines: KStream[String, String] = builder.stream[String, String]("test_topic")
@@ -26,11 +30,21 @@ object MapFunctionScalaExample extends App {
     (key, value) => println(s"key: $key, value: $value")
   )
 
+  val wordCounts: KTable[String, Long] = textLines
+    .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
+    .groupBy((_, word) => word)
+    .count()
+  wordCounts.toStream.to("test_topic-wordcount")
+
   val streams: KafkaStreams = new KafkaStreams(builder.build(), config)
+
+  streams.cleanUp() // Just a test!
   streams.start()
 
+
+  // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
   sys.ShutdownHookThread {
-    streams.close(Duration.ofSeconds(10))
-  }
+   streams.close(Duration.ofSeconds(10))
+ }
 
 }

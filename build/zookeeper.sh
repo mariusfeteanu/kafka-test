4c151cfa30fb40a8d2c3cf2ded9badd6bacba4ea
Marius Feteanu <marius.feteanu@gmail.com>
2019-07-02 10:35:34 +0100

    something changed, what?

--- build/zookeeper.sh
+++ build/zookeeper.sh
@@ -1,5 +1,5 @@
 cd /root
-wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz 
+wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz
 tar -zxf zookeeper-3.4.9.tar.gz
 mv zookeeper-3.4.9 /usr/local/zookeeper
 mkdir -p /var/lib/zookeeper
